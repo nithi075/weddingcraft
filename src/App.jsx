1cbe85ad8d2cf835e73ce8782083b1bdbd5b96a0
nithi075 <nithishraj8055@gmail.com>
2026-04-23 14:35:03 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,33 @@ import "./App.css";
 
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
-import Portfolio from "./components/Portfolio/Portfolio";
 import Packages from "./components/packages/Packages";   // NEW
 import Featured from "./components/featured/featured";
 import About from "./components/About/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Packages from "./components/packages/Packages";   // NEW
 import Testimonials from "./components/testimonials/Testimonials";
 import Instagram from "./components/instagram/Instagram";
 import Blog from "./components/blog/blog";
 import Footer from "./components/footer/footer";
 import Gallery from "./pages/Gallery";
 import Location from "./components/location/Location";
+import Featured from "./components/featured/featured";
+import Services from "./components/Services/Services"
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       {/* Home Section */}
       <section id="home">
-        <Hero />
+        <Hero/>
       </section>
 
       {/* Services Section */}
       <section id="services">
-        <Services />
+         <Services/>
       </section>
 
        <section id="gallery">
@@ -33,12 +38,12 @@ function App() {
 
       {/* Gallery Section */}
       <section id="gallery">
-        <Featured />
+        <Featured/>
       </section>
 
       {/* About Section */}
       <section id="about">
-        <About />
+        <About/>
       </section>
 
       {/* Video Section */}
